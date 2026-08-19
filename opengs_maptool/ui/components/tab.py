f5deacbadc91031d069687e2148c5ae6f881baea
GermanCodeEngineer <germancodeengineer@proton.me>
2026-08-19 18:20:44 +0200

    refactor(ui): move tab refresh responsibilities into task flow and create tab name enum to replace literals

--- opengs_maptool/ui/components/tab.py
+++ opengs_maptool/ui/components/tab.py
@@ -1,8 +1,9 @@
 from PyQt6.QtWidgets import QVBoxLayout, QWidget
+from opengs_maptool.simple_types import TabName
 from opengs_maptool.ui.image_display import ImageDisplay
 
 class Tab(QWidget):
-    def __init__(self, tab_name):
+    def __init__(self, tab_name_enum: TabName):
         super().__init__()
 
         # Create image display
@@ -10,13 +11,12 @@ class Tab(QWidget):
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self._image_display)
 
-        self._tab_name = tab_name
+        self._tab_name_enum = tab_name_enum
 
 
-    def get_image_display(self):
+    def get_image_display(self) -> ImageDisplay:
         return self._image_display
-    
 
-    def get_tab_name(self):
-        return self._tab_name
-    
+
+    def get_tab_name_enum(self) -> TabName:
+        return self._tab_name_enum
